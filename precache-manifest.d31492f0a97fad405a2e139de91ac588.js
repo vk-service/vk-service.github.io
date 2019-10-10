2e436e9a6f1c111aa60af7f8aa84acfc3dbc25cf
vk-service <56194695+vk-service@users.noreply.github.com>
2019-10-10 10:33:43 +0300

    Add files via upload

---
+++ precache-manifest.d31492f0a97fad405a2e139de91ac588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dc5d7601e04ca4e1e2027c9894dd530",
+    "url": "/static/media/skachat-stikery-artyom-dlya-telegram-6.8dc5d760.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "481cdad37f1290b4150d",
+    "url": "/static/js/main.47024644.chunk.js"
+  },
+  {
+    "revision": "dc88a85a39bd77595c68",
+    "url": "/static/js/2.9034f697.chunk.js"
+  },
+  {
+    "revision": "481cdad37f1290b4150d",
+    "url": "/static/css/main.44ba9b8a.chunk.css"
+  },
+  {
+    "revision": "dc88a85a39bd77595c68",
+    "url": "/static/css/2.36398225.chunk.css"
+  },
+  {
+    "revision": "ee7eca63d55d826e9c45906dbaee307f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b8684695311203bc574d63154f9a0b1.js"
+  "/precache-manifest.d31492f0a97fad405a2e139de91ac588.js"
 );
 
 workbox.clientsClaim();
