9a118e9a7c90a40f0827a42735661722996c98bf
vk-service <56194695+vk-service@users.noreply.github.com>
2019-11-21 19:48:02 +0300

    Add files via upload

---
+++ precache-manifest.fa1c84cbcea3b8804e1346a54a0db32f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "52172be44e4b4494f2a861a6fba4294c",
+    "url": "./static/media/template.52172be4.png"
+  },
+  {
+    "revision": "8dc5d7601e04ca4e1e2027c9894dd530",
+    "url": "./static/media/skachat-stikery-artyom-dlya-telegram-6.8dc5d760.png"
+  },
+  {
+    "revision": "57c712aa0c348a14f2e12ee6439ac99c",
+    "url": "./static/media/santa.57c712aa.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7e3e6e870d00df039ddb",
+    "url": "./static/js/main.6bba7cd3.chunk.js"
+  },
+  {
+    "revision": "4b53e003d8a824f2aedc",
+    "url": "./static/js/2.c6ecd0b1.chunk.js"
+  },
+  {
+    "revision": "7e3e6e870d00df039ddb",
+    "url": "./static/css/main.b3e24fd0.chunk.css"
+  },
+  {
+    "revision": "4b53e003d8a824f2aedc",
+    "url": "./static/css/2.20df0b0b.chunk.css"
+  },
+  {
+    "revision": "f0b82d42b502761d94a3f3bc96b4b7c8",
+    "url": "./index.html"
+  }
+];
